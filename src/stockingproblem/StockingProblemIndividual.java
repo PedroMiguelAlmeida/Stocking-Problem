23618397ad3fd9d61fe4793edfc0554ac8e6847e
¨PedroMiguelAlmeida¨ <pedromiguel.almd@gmail.com>
2021-04-30 08:54:27 +0100

    Starting commit

---
+++ src/stockingproblem/StockingProblemIndividual.java
@@ -0,0 +1,66 @@
+package stockingproblem;
+
+import algorithms.IntVectorIndividual;
+
+public class StockingProblemIndividual extends IntVectorIndividual<StockingProblem, StockingProblemIndividual> {
+    //TODO this class might require the definition of additional methods and/or attributes
+
+    public StockingProblemIndividual(StockingProblem problem, int size) {
+        super(problem, size);
+        //TODO
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    public StockingProblemIndividual(StockingProblemIndividual original) {
+        super(original);
+        //TODO
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    @Override
+    public double computeFitness() {
+        //TODO
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    private boolean checkValidPlacement(Item item, int[][] material, int lineIndex, int columnIndex) {
+        int[][] itemMatrix = item.getMatrix();
+        for (int i = 0; i < itemMatrix.length; i++) {
+            for (int j = 0; j < itemMatrix[i].length; j++) {
+                if (itemMatrix[i][j] != 0) {
+                    if ((lineIndex + i) >= material.length
+                            || (columnIndex + j) >= material[0].length
+                            || material[lineIndex + i][columnIndex + j] != 0) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("fitness: ");
+        sb.append(fitness);
+        //TODO
+        return sb.toString();
+    }
+
+    /**
+     * @param i
+     * @return 1 if this object is BETTER than i, -1 if it is WORST than I and
+     * 0, otherwise.
+     */
+    @Override
+    public int compareTo(StockingProblemIndividual i) {
+        return (this.fitness == i.getFitness()) ? 0 : (this.fitness < i.getFitness()) ? 1 : -1;
+    }
+
+    @Override
+    public StockingProblemIndividual clone() {
+        return new StockingProblemIndividual(this);
+
+    }
+}
